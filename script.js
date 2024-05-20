8324281a7ef1a7787309d3303424854b677def94
Sebastian Pereira <157150645+sebapereira111@users.noreply.github.com>
2024-05-20 17:31:49 -0400

    Archivo script completo

--- script.js
+++ script.js
@@ -4,3 +4,43 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => Math.floor(Math.random()*10);
+
+const compareGuesses = (humanGuess, computerGuess, secretTarget) => Math.abs(humanGuess-secretTarget) > Math.abs(computerGuess-secretTarget) ? false : true;
+
+const updateScore = (winner) => winner === 'human' ? humanScore += 1 : computerScore += 1;
+
+const advanceRound = () => currentRoundNumber += 1;
+
+/* seccion solo para probar las funciones
+
+function test () {
+    const target = generateTarget();
+    const human = generateTarget();
+    const computer = generateTarget();
+
+    console.log('Ronda ' + currentRoundNumber);
+    advanceRound();
+
+    console.log('Target: ' + target);
+    console.log('Human: ' + human);
+    console.log('Computer: ' + computer);
+
+    if (compareGuesses(human, computer, target)) {
+        updateScore('human');
+    } else {
+        updateScore('computer');
+    }
+
+    console.log('Human score: ' + humanScore);
+    console.log('Computer score: ' + computerScore);
+    console.log('');
+}
+
+test();
+test();
+test();
+test();
+test();
+
+*/
